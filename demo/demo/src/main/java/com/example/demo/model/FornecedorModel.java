99096a2fbe4ff56c5a29355a7d625225fd890126
Felipe Galeti Gôngora <fgaletigongora@gmail.com>
2026-03-22 15:25:01 -0300

    Add FornecedorModel

---
+++ demo/demo/src/main/java/com/example/demo/model/FornecedorModel.java
@@ -0,0 +1,61 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "Fornecedor")
+@Table(name = "tb_fornecedor")
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_fantasia")
+    private String nomeFantasia;
+
+    @Column(name = "nr_cnpj")
+    private String cnpj;
+
+    @Column(name = "ds_contato")
+    private String contato;
+
+    public Fornecedor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
